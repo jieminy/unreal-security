a3c8fc72354379ab8b20a18968315e08945bb6db
jieminy <jiemin.y@163.com>
2018-08-28 00:00:54 +0800

    -add spring security userDetailsService

--- unreal-security-browser/src/main/java/com/unreal/security/browser/config/SecurityConfig.java
+++ unreal-security-browser/src/main/java/com/unreal/security/browser/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.unreal.security.browser.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @Author: JieMin
@@ -11,13 +14,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
-                .loginPage("/imooc-signIn.html")
+                .loginPage("/login.html")
                 .and()
                 .authorizeRequests()        // 定义哪些URL需要被保护、哪些不需要被保护
-                .antMatchers("/imooc-signIn.html").permitAll()
+                .antMatchers("/login.html").permitAll()
                 .anyRequest()               // 任何请求,登录后可以访问
                 .authenticated();
     }
